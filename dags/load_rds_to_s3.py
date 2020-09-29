629eeebb2b7a9b5eeeb06cd7805b3868b97a2e9e
Kaavya Krishna-Kumar <KaavyaKrishna-Kumar@Kaavyas-MacBook-Pro.local>
2020-09-29 17:34:40 -0400

    edited postgres method

--- dags/load_rds_to_s3.py
+++ dags/load_rds_to_s3.py
@@ -8,6 +8,7 @@ from airflow.operators.python_operator import PythonOperator
 
 from tempfile import TemporaryFile
 
+import pandas as pd 
 
 
 
@@ -23,14 +24,16 @@ def get_postgres_data(tempfile):
     request = "SELECT * FROM blocks LIMIT 10" #double check how to write this
     pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
     connection = pg_hook.get_conn() #gets the connection from postgres
-    result = connection.get_pandas_df(request)
-    result.to_csv(tempfile)
+    #result = connection.get_pandas_df(request)
+    #result.to_csv(tempfile)
 
-    #cursor = connection.cursor() #cursor to postgres database
-    #cursor.execute(request) #executes request
-    #sources = cursor.fetchall() #fetches all the data from the executed request
+    cursor = connection.cursor() #cursor to postgres database
+    cursor.execute(request) #executes request
+    sources = cursor.fetchall() #fetches all the data from the executed request
+    df = pd.DataFrame(sources)
+    df.to_csv('tempfile')
 
-def upload_data_to_S3(tempfile, bucket_name):
+def upload_data_to_S3(file_name, bucket_name):
     hook = airflow.hooks.S3_hook.S3_hook('s3_conn')
     hook.load_file(file_name, bucket_name)
 
