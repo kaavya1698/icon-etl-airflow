239952760986647d855398e6b2bc1a684fdcdec2
kaavya1698 <kaavya1698@gmail.com>
2020-10-01 15:36:29 -0400

    all in one file

--- dags/load_rds_to_s3.py
+++ dags/load_rds_to_s3.py
@@ -21,30 +21,27 @@ default_args = {
 }
 
 def get_postgres_data():
-    get_date(ds, **kwargs)
-    request = "SELECT * FROM (SELECT *,  timestamp 'epoch' + CAST(timestamp AS BIGINT)/1000000 * interval '1 second' AS date_timestamp FROM blocks) AS a WHERE a.date_timestamp :: date = date '%s'" #double check how to write this
+    request = "SELECT * FROM blocks" #double check how to write this
     pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
     connection = pg_hook.get_conn() #gets the connection from postgres
     cursor = connection.cursor() #cursor to postgres database
     cursor.execute(request %execution_date) #executes request
     sources = cursor.fetchall() #fetches all the data from the executed request
     results = pd.DataFrame(sources) #writes to datafram
-    print(execution_date)
     print(results)
     results.to_csv('/home/ubuntu/s3_dump/test.csv') #printing to dir owned by airflow. Need to change this to temp dir but can be done later
 
 
 
 def upload_data_to_S3(filename, key, bucket_name):
-    get_date(ds, **kwargs)
     hook = S3Hook('s3_conn')
-    hook.load_file(filename=filename, key=execution_date + key, bucket_name=bucket_name)
+    hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
 
 def get_date(ds, **kwargs):
     Variable.set('execution_date', kwargs['execution_date'])
     return
 
-with DAG('load_rds_s3', default_args=default_args, schedule_interval = '0 8 * * *', catchup=True) as dag:
+with DAG('load_rds_s3', default_args=default_args, schedule_interval = '0 8 * * *', catchup=False) as dag:
 
     start_task = DummyOperator(task_id = 'start_task')
     load_rds_task = PythonOperator(task_id='load_rds', python_callable = get_postgres_data)
