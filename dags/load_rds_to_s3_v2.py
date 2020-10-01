14213c44b4d0e1accd32ff032c88d910a6d0ad98
Kaavya Krishna-Kumar <KaavyaKrishna-Kumar@Kaavyas-MacBook-Pro.local>
2020-09-30 22:41:56 -0400

    redshift dag

--- dags/load_rds_to_s3_v2.py
+++ dags/load_rds_to_s3_v2.py
@@ -6,10 +6,9 @@ from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryFile
 
-import pandas as pd
-import os  
+import pandas as pd 
 
 
 
@@ -21,7 +20,7 @@ default_args = {
     'retry_delay' : timedelta(minutes=5)
 }
 
-def get_postgres_data(tempdir):
+def get_postgres_data():
     request = "SELECT * FROM blocks LIMIT 25" #double check how to write this
     pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
     connection = pg_hook.get_conn() #gets the connection from postgres
@@ -33,7 +32,8 @@ def get_postgres_data(tempdir):
     sources = cursor.fetchall() #fetches all the data from the executed request
     results = pd.DataFrame(sources)
     print(results)
-    results.to_csv('/result_dir/tempfile.csv')
+    results.to_csv('/home/ubuntu/s3_dump/test.csv')
+
 
 
 def upload_data_to_S3(filename, key, bucket_name):
@@ -42,17 +42,21 @@ def upload_data_to_S3(filename, key, bucket_name):
 
 
 def run_export_to_s3():
-    result_dir = TemporaryDirectory()
-    get_postgres_data('result_dir')
-    upload_data_to_S3('/result_dir/tempfile.csv', 'my_s3_file_v2.csv', 'icon-redshift-dump-dev')
+    get_postgres_data()
+    upload_data_to_S3("~/tempfile.csv", "icon-redshift-dump-dev")
     #delete tempfile.csv
 
 
-with DAG('load_rds_s3_v2', default_args=default_args, schedule_interval = "@once", catchup=False) as dag:
+with DAG('load_rds_s3', default_args=default_args, schedule_interval = '0 8 * * *', catchup=False) as dag:
 
 
     start_task = DummyOperator(task_id = 'start_task')
-    #load_rds_task = PythonOperator(task_id='load_rds', python_callable = get_postgres_data)
-    #upload_to_s3_task = PythonOperator(task_id='upload_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/test.csv', 'key':'my_s3_file.csv', 'bucket_name': 'icon-redshift-dump-dev'})
-    run_export_to_s3 = PythonOperator(task_id = 'run_export_to_s3', python_callable = run_export_to_s3)
-    start_task >> run_export_to_s3
+    load_rds_task = PythonOperator(task_id='load_rds', python_callable = get_postgres_data)
+    upload_to_s3_task = PythonOperator(task_id='upload_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/test.csv', 'key':'my_s3_file.csv', 'bucket_name': 'icon-redshift-dump-dev'})
+    start_task >> load_rds_task >> upload_to_s3_task
+
+
+
+
+
+
---
+++ dags/s3_to_Redshift.py
@@ -0,0 +1,29 @@
+import datetime as dt
+ 
+from airflow import DAG
+from airflow.operators.s3_to_redshift_operator import S3ToRedshiftTransfer
+ 
+default_args = {
+    'owner' : 'airflow',
+    'depend_on_past' : False,
+    'start_date' : datetime(2020, 9, 28, 00, 00, 00),
+    'retries': 5,
+    'retry_delay' : timedelta(minutes=5)
+}
+ 
+dag = DAG('redshift-etl',
+  default_args=default_args,
+  schedule_interval='@once'
+)
+
+transfer_redshift= S3ToRedshiftTransfer(
+    task_id='transfer_redshift',
+    schema='schema',
+    table= 'block_test',
+    s3_bucket='icon-redshift-dump-dev',
+    redshift_conn_id = 'icon-analytics-dev',
+    default_args= 'default_args'
+    dag=dag
+)
+
+transfer_redshift
