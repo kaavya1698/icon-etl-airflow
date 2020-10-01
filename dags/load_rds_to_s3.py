23bf69fe892c9a17cc5f4a52745c272b2140f65a
kaavya1698 <kaavya1698@gmail.com>
2020-10-01 14:20:54 -0400

    fixing sql

--- dags/load_rds_to_s3.py
+++ dags/load_rds_to_s3.py
@@ -20,7 +20,7 @@ default_args = {
 }
 
 def get_postgres_data():
-    request = "SELECT *, timestamp 'epoch' + CAST(timestamp AS BIGINT)/1000000 * interval '1 second' AS date_timestamp FROM blocks WHERE a.date_timestamp :: date = '%s' LIMIT 25;"%(start_date) #double check how to write this
+    request = "SELECT *, timestamp 'epoch' + CAST(timestamp AS BIGINT)/1000000 * interval '1 second' AS date_timestamp FROM blocks WHERE date_timestamp :: date = '%s' LIMIT 25;"%(start_date) #double check how to write this
     pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
     connection = pg_hook.get_conn() #gets the connection from postgres
     cursor = connection.cursor() #cursor to postgres database
