ce243bf37d52b5e17dc8a4a724e17c4d46b7ffc4
kaavya1698 <kaavya1698@gmail.com>
2020-10-02 11:58:30 -0400

    testing

--- dags/s3_to_redshift2.py
+++ dags/s3_to_redshift2.py
@@ -2,13 +2,16 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.s3_to_redshift_operator import S3ToRedshiftTransfer
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import Variable
+
+start_date = Variable.get("s3_redshift_start_date")
 
 default_args = {
-	'owner': 'airflow',
-	'depends_on_past' : False,
-	'start_date' : datetime(2020, 9, 28, 00, 00, 00),
-	'retries' : 5,
-	'retry_delay' : timedelta(minutes=5)
+    'owner' : 'airflow',
+    'depend_on_past' : False,
+    'start_date' : start_date,
+    'retries': 5,
+    'retry_delay' : timedelta(minutes=5)
 }
 
 dag = DAG('upload_redshift', default_args=default_args, schedule_interval = '0 8 * * *', catchup=False)
