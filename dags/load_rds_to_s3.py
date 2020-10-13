7ff1d6e6b4ca9c9a2e032ddd41b6cc1d15462a09
kaavya1698 <kaavya1698@gmail.com>
2020-10-12 22:08:23 -0400

    added all tables

--- dags/load_rds_to_s3.py
+++ dags/load_rds_to_s3.py
@@ -43,6 +43,28 @@ def get_postgres_transactions_data():
     print(trans_results)
     trans_results.to_csv('/home/ubuntu/s3_dump/transactions_dump.csv') #printing to dir owned by airflow. Need to change this to temp dir but can be done later
 
+def get_postgres_receipts_data():
+    receipts_request = "SELECT * FROM receipts" #double check how to write this
+    pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
+    connection = pg_hook.get_conn() #gets the connection from postgres
+    receipts_cursor = connection.cursor() #cursor to postgres database
+    receipts_cursor.execute(receipts_request) #executes request
+    receipts_sources = receipts_cursor.fetchall() #fetches all the data from the executed request
+    receipts_results = pd.DataFrame(receipts_sources) #writes to datafram
+    print(receipts_results)
+    receipts_results.to_csv('/home/ubuntu/s3_dump/receipts_dump.csv') #printing to dir owned by airflow. Need to change this to temp dir but can be done later
+
+def get_postgres_logs_data():
+    logs_request = "SELECT * FROM logs" #double check how to write this
+    pg_hook = PostgresHook(postgres_conn_id="postgres", schema="postgres") #made this connection in Airflow UI
+    connection = pg_hook.get_conn() #gets the connection from postgres
+    logs_cursor = connection.cursor() #cursor to postgres database
+    logs_cursor.execute(logs_request) #executes request
+    logs_sources = logs_cursor.fetchall() #fetches all the data from the executed request
+    logs_results = pd.DataFrame(logs_sources) #writes to datafram
+    print(logs_results)
+    logs_results.to_csv('/home/ubuntu/s3_dump/logs_dump.csv') #printing to dir owned by airflow. Need to change this to temp dir but can be done later
+
 def upload_data_to_S3(filename, key_prefix, key, bucket_name):
     hook = S3Hook('s3_conn')
     hook.load_file(filename=filename, key=key_prefix + key, bucket_name=bucket_name)
@@ -53,14 +75,27 @@ with DAG('load_rds_s3', default_args=default_args, schedule_interval = '@once', 
     start_task = DummyOperator(task_id = 'start_task')
     end_task = DummyOperator(task_id = 'end_task')
 
+    #blocks
     load_block_rds_task = PythonOperator(task_id='load_block_rds', python_callable = get_postgres_block_data)
-    load_transactions_rds_task = PythonOperator(task_id='load_transactions_rds', python_callable = get_postgres_transactions_data)
-
     upload_blocks_to_s3_task = PythonOperator(task_id='upload_blocks_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/blocks_dump.csv','key_prefix':'blocks', 'key':'_rds_dump', 'bucket_name': 'icon-redshift-dump-dev'})
+
+    #transactions
+    load_transactions_rds_task = PythonOperator(task_id='load_transactions_rds', python_callable = get_postgres_transactions_data)
     upload_transactions_to_s3_task = PythonOperator(task_id='upload_transactions_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/transactions_dump.csv','key_prefix':'transactions', 'key':'_rds_dump', 'bucket_name': 'icon-redshift-dump-dev'})
+
+    #receipts
+    load_receipts_rds_task = PythonOperator(task_id='load_receipts_rds', python_callable = get_postgres_receipts_data)
+    upload_receipts_to_s3_task = PythonOperator(task_id='upload_receipts_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/receipts_dump.csv','key_prefix':'receipts', 'key':'_rds_dump', 'bucket_name': 'icon-redshift-dump-dev'})
+
+    #logs
+    load_logs_rds_task = PythonOperator(task_id='load_logs_rds', python_callable = get_postgres_logs_data)
+    upload_logs_to_s3_task = PythonOperator(task_id='upload_logs_to_S3', python_callable = upload_data_to_S3, op_kwargs={'filename': '/home/ubuntu/s3_dump/logs_dump.csv','key_prefix':'logs', 'key':'_rds_dump', 'bucket_name': 'icon-redshift-dump-dev'})
+
 
     start_task >> load_block_rds_task >> upload_blocks_to_s3_task >> end_task
     start_task >> load_transactions_rds_task >> upload_transactions_to_s3_task >> end_task
+    start_task >> load_receipts_rds_task >> upload_receipts_to_s3_task >> end_task
+    start_task >> load_logs_rds_task >> upload_logs_to_s3_task >> end_task
 
 
 
